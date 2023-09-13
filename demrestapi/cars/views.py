45089587f2cf1a854f21cb42ba5f0be02097bb25
Eugeniy <zhenya.0295@mail.ru>
2023-09-14 01:44:16 +0300

    init web-site for django rest api

---
+++ demrestapi/cars/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from .serializers import CarDetailSerializer, CarsListSerializer
+from .models import Car
+from . permission import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.authentication import TokenAuthentication
+
+
+class CarCreateView(generics.CreateAPIView):
+    serializer_class = CarDetailSerializer
+
+
+class CarsListView(generics.ListAPIView):
+    serializer_class = CarsListSerializer
+    queryset = Car.objects.all()
+    permission_classes = (IsAdminUser, )
+
+
+class CarDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CarDetailSerializer
+    queryset = Car.objects.all()
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly, IsAdminUser)
